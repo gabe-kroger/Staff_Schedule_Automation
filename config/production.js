027d1701531a8560d3c1c3ff0f47e146aca168f5
Gabriel Kroger <gbkroger@ualr.edu>
2022-05-04 17:08:11 -0500

    Prepare for deploy

---
+++ config/production.js
@@ -0,0 +1,4 @@
+{
+  "mongoURI": "mongodb+srv://gabe123:gabe123@cluster0.txvts.mongodb.net/test?retryWrites=true&w=majority",
+  "jwtSecret": "mysecrettoken"
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db'); //we're importing connectDB from config/db.js
 const app = express();
+const path = require('path');
 //connect database
 connectDB();
 //Init Middleware for parser
@@ -14,6 +15,16 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/instructors', require('./routes/api/instructors'));
 app.use('/api/courses', require('./routes/api/courses'));
 app.use('/api/schedule', require('./routes/api/schedule'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.port || 5000;
 
