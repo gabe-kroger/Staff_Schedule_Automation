3670c23eacbc4ac58e66b17df9769895a536b253
Kush Patel <khpatel180@gmail.com>
2022-05-06 00:56:07 -0500

    schema error

--- models/Error.js
+++ models/Error.js
@@ -2,9 +2,7 @@
 
 const mongoose = require('mongoose'); //importing mongoose
 const ErrorSchema = new mongoose.Schema({
-  error: {
-    type: String,
-  },
+  error: String,
 });
 
 module.exports = Error = mongoose.model('error', ErrorSchema); //exporting CourseSchema model
